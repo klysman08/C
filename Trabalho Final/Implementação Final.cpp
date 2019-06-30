880dc4ac0739e445b1f6374772d106b199c388b9
Klysman <klysman.rk@gmail.com>
2019-06-30 18:26:49 -0300

    Correções

--- Trabalho Final/Implementação Final.cpp
+++ Trabalho Final/Implementação Final.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <vector>
-#include <ctime>> //para o sorteio e regar o código de validacao
+#include <ctime>   //para o sorteio e regar o código de validacao
 #include <cstdlib> //para o sorteio e regar o código de validacao
 
 using namespace std;
@@ -102,14 +102,12 @@ class Cinema : public Evento
 {
 
 public:
-
     string tipo;
     string filme;
     int sala;
     string data;
     string horario;
     float valor_cine = 20;
-
 
     Cinema() // contrutor padrao
     {
@@ -144,19 +142,19 @@ public:
             cout << "Filme:" << lista_filmes[i].filme << endl;
             cout << "Data:" << lista_filmes[i].data << endl;
             cout << "Horario:" << lista_filmes[i].horario << endl;
-            cout << "Valor:" << this->valor_cine << endl << ",00 R$";
+            cout << "Valor:" << this->valor_cine << endl
+                 << ",00 R$";
             //cout << "Codigo:" << this->codigo_cine << endl;
 
             cout << "\n====================================================\n";
         }
     }
-
 };
 
 class BilheteCinema : public Cinema
 {
 public:
-    int codigo_cine = (rand()%1000)+1;
+    int codigo_cine = (rand() % 10000000000) + 1; // Gera o codigo de validacao
 
     void emite_bilhete_filme(const vector<Cinema> &lista_filmes) // Funcao que imprime os atributos dos objetos instanciados via <vector>
     {
@@ -174,8 +172,6 @@ public:
             cout << "\n====================================================\n";
         }
     }
-
-
 };
 
 class Show : public Evento
@@ -212,7 +208,7 @@ public:
 class BilheteShow : public Show
 {
 public:
-    int codigo_sw = (rand()%1000)+1;
+    int codigo_sw = (rand() % 10000000000) + 1; // Gera o codigo de validacao
 
     void emite_bilhete_show(const vector<Show> &lista_show) // Funcao que imprime os atributos dos objetos instanciados via <vector>
     {
@@ -268,7 +264,7 @@ public:
 
 class BilheteTeatro : public Teatro
 {
-    int codigo_te = (rand()%1000)+1;
+    int codigo_te = (rand() % 10000000000) + 1; // Gera o codigo de validacao
 
 public:
     void emite_bilhete_teatro(const vector<Teatro> &lista_teatro) // Funcao que imprime os atributos dos objetos instanciados via <vector>
@@ -300,7 +296,7 @@ public:
         int idade;
         int cpf;
 
-        cout << "Informe os dados para cadastro no sistema \n";
+        cout << "Informe os dados para cadastro no sistema. (somente o primeiro nome) \n";
         cout << "Seu nome: ";
         cin >> nome;
         cout << "Sua idade: ";
@@ -317,21 +313,40 @@ public:
     Cinema inscreverFilme()
     {
         string tipo;
+        int sala;
         string filme;
         string data;
         string horario;
 
         cout << "Informe os dados do Filme que deseja\n\n";
         cout << "Legendado, Legendado 3D, Dublado, Dublado 3D: ";
+        cout << "\n1 - Legendado\n2 - Legendado 3D\n3 - Dublado\n4 - Dublado 3D\n";
         cin >> tipo;
-        cout << "Informe a sala: ";
+        cout << "Informe a sala (1,2 ou 3): ";
         cin >> sala;
-        cout << "Informe o nome do filme: ";
+        cout << "Informe o nome do filme (somente o primeiro nome): ";
         cin >> filme;
-        cout << "Informe a data desejada: ";
+        cout << "Informe a data desejada (exemplo 20/07/2019): ";
         cin >> data;
-        cout << "Informe o horario: ";
+        cout << "Informe o horario (exemplo 20h): ";
         cin >> horario;
+
+        if (tipo == "1")
+        {
+            tipo = "Legendado";
+        }
+        if (tipo == "2")
+        {
+            tipo = "Legendado 3D";
+        }
+        if (tipo == "3")
+        {
+            tipo = "Dublado";
+        }
+        if (tipo == "4")
+        {
+            tipo = "Dublado 3D";
+        }
 
         Cinema *a1 = new Cinema(tipo, sala, filme, data, horario);
 
@@ -347,13 +362,13 @@ public:
         string horario;
 
         cout << "Informe os dados do Show que deseja\n\n";
-        cout << "Qual o setor? ";
+        cout << "Qual o setor? (1,2 ou 3): ";
         cin >> setor;
-        cout << "Informe a atracao: (rock, pop, kpop, axe, sertanejo, country): ";
+        cout << "Informe a atracao (rock, pop, kpop, axe, sertanejo, country): ";
         cin >> atracao;
-        cout << "Informe a data: ";
+        cout << "Informe a data (exemplo 20/07/2019): ";
         cin >> data;
-        cout << "Informe o horario: ";
+        cout << "Informe o horario (exemplo 20h): ";
         cin >> horario;
         Show *b1 = new Show(setor, atracao, data, horario);
 
@@ -370,16 +385,30 @@ public:
         string horario;
 
         cout << "Informe os dados do Teatro que deseja\n\n";
-        cout << "Qual o tipo? (musical, opera, filarmonica): ";
+        cout << "Qual o tipo de obra voce gostaria?";
+        cout << "\n1 - Musical\n2 - Opera\n3 - Filarminica\n";
         cin >> tipo;
-        cout << "Informe o nome da peca desejada: ";
+        cout << "Informe o nome da peca desejada:";
         cin >> nome;
-        cout << "Informe o numero da sala: ";
+        cout << "Informe o numero da sala (1,2 ou 3): ";
         cin >> sala;
-        cout << "Informe a data: ";
+        cout << "Informe a data (exemplo 20/07/2019): ";
         cin >> data;
-        cout << "Informe o horario: ";
+        cout << "Informe o horario (exemplo 20h): ";
         cin >> horario;
+
+        if (tipo == "1")
+        {
+            tipo = "Musical";
+        }
+        if (tipo == "2")
+        {
+            tipo = "Opera";
+        }
+        if (tipo == "3")
+        {
+            tipo = "Filarmonica";
+        }
 
         Teatro *t1 = new Teatro(tipo, nome, sala, data, horario);
 
@@ -395,11 +424,17 @@ void emissao_bilhete()
     cin >> escolha_emissao;
     if (escolha_emissao == 1)
     {
-        cout << "Bilhete emitido com sucesso.Finalizando processo...\nProcesso finalizado\n\n\n";
+        cout << "Bilhete emitido com sucesso. Finalizando processo...\nProcesso finalizado\n\n\n";
+        cout << "=============================================\n";
+        cout << "=============================================\n";
+        cout << "=============================================\n";
     }
     else
     {
         cout << "Processo finalizado sem a emissao do bilhete.\n\n\n";
+        cout << "=============================================\n";
+        cout << "=============================================\n";
+        cout << "=============================================\n";
     }
 }
 
@@ -428,18 +463,18 @@ int main()
     vector<Teatro> teatro;
 
     // Variaveis responsasveis para genrenciar a interface do usario.
-    int escolha; //Usada para inicializar ou finalizar a máquina:1-Inicializa;9-Finaliza.
-    int escolhe_evento; //Escolhe o evento ao qual o bilhete corresponde:1-Cinema;2-Show;3-Teatro.
-    int escolhe_pagamento; //Escolhe o metodo de pagamento: 1-Dinheiro;2-Crédito;3-Débito;4-Bilhete de cortesia.
-    float valor_cine; //Usada para comparação entre o valor pago e o valor do bilhete para confirmar o pagamento e gerar o troco.
-    float valor_show; //Usada para comparação entre o valor pago e o valor do bilhete para confirmar o pagamento e gerar o troco.
-    float valor_teatro; //Usada para comparação entre o valor pago e o valor do bilhete para confirmar o pagamento e gerar o troco.
-    long int num_cartao; //Usada para os cartões de crédito e débito.
-    string bandeira; //Usada para os cartões de crédito e débito.
-    long int confirma_cpf; //Usada para o bilhete de cortesia.
+    int escolha;               //Usada para inicializar ou finalizar a máquina:1-Inicializa;9-Finaliza.
+    int escolhe_evento;        //Escolhe o evento ao qual o bilhete corresponde:1-Cinema;2-Show;3-Teatro.
+    int escolhe_pagamento;     //Escolhe o metodo de pagamento: 1-Dinheiro;2-Crédito;3-Débito;4-Bilhete de cortesia.
+    float valor_cine;          //Usada para comparação entre o valor pago e o valor do bilhete para confirmar o pagamento e gerar o troco.
+    float valor_show;          //Usada para comparação entre o valor pago e o valor do bilhete para confirmar o pagamento e gerar o troco.
+    float valor_teatro;        //Usada para comparação entre o valor pago e o valor do bilhete para confirmar o pagamento e gerar o troco.
+    long int num_cartao;       //Usada para os cartões de crédito e débito.
+    string bandeira;           //Usada para os cartões de crédito e débito.
+    long int confirma_cpf;     //Usada para o bilhete de cortesia.
     long int bilhete_cortesia; //Usada para armazenar o numero do bilhete de cortesia
 
-    while (1)//Programa roda indefinidamente, até que o valor de escolha seja alterado para 9.
+    while (1) //Programa roda indefinidamente, até que o valor de escolha seja alterado para 9.
     {
         cout << "Bem vindo ao sistema para compra de ingressos.\n\nTemos a venda ingressos para Cinema, Shows e Teatro.\nPressione o botao 1 se voce deseja iniciar o processo de compras.\n";
         cin >> escolha;
@@ -460,21 +495,21 @@ int main()
             {
                 *a1 = s1->inscreverFilme(); // Chama a funcao na classe sistema para instanciar os objetos
                 a1->nome_ev = ("Cinema");
-                cine.push_back(*a1);        // Armazena o objeto criado na linha de cima em uma lista do tipo vector.
+                cine.push_back(*a1); // Armazena o objeto criado na linha de cima em uma lista do tipo vector.
             }
 
             if (escolhe_evento == 2)
             {
                 *b1 = s1->inscreverShow(); // Chama a funcao na classe sistema para instanciar os objetos
                 b1->nome_ev = ("Show");
-                show.push_back(*b1);       // Armazena o objeto criado na linha de cima em uma lista do tipo vector.
+                show.push_back(*b1); // Armazena o objeto criado na linha de cima em uma lista do tipo vector.
             }
 
             if (escolhe_evento == 3)
             {
                 *t1 = s1->inscreverTeatro(); // Chama a funcao na classe sistema para instanciar os objetos
                 t1->nome_ev = ("Teatro");
-                teatro.push_back(*t1);       // Armazena o objeto criado na linha de cima em uma lista do tipo vector.
+                teatro.push_back(*t1); // Armazena o objeto criado na linha de cima em uma lista do tipo vector.
             }
 
             // Sessão do programa para o pagamento;
@@ -503,8 +538,7 @@ int main()
                     if (valor_cine >= a1->getvalor())
                     {
                         cout << "Pagamento realizado com sucesso!";
-                        cout << "\nSeu troco eh: "<< valor_cine - a1->getvalor();
-
+                        cout << "\nSeu troco eh: " << valor_cine - a1->getvalor();
                     }
 
                     cout << "\n\n\nConfirme suas informacoes antes de emitir o bilhete.\n\n";
@@ -527,8 +561,7 @@ int main()
                     if (valor_show >= b1->getvalor())
                     {
                         cout << "Pagamento realizado com sucesso!";
-                        cout << "\nSeu troco eh: "<< valor_show - b1->getvalor();
-
+                        cout << "\nSeu troco eh: " << valor_show - b1->getvalor();
                     }
 
                     cout << "\n\n\nConfirme suas informacoes antes de emitir o bilhete.\n\n";
@@ -551,8 +584,7 @@ int main()
                     if (valor_teatro >= t1->getvalor())
                     {
                         cout << "Pagamento realizado com sucesso!";
-                        cout << "\nSeu troco eh: "<< valor_teatro - t1->getvalor();
-
+                        cout << "\nSeu troco eh: " << valor_teatro - t1->getvalor();
                     }
 
                     cout << "\n\n\nConfirme suas informacoes antes de emitir o bilhete.\n\n";
@@ -586,7 +618,6 @@ int main()
 
             */
 
-
             /* Se for cartão de crédito, o usuário deve fornecer o número do cartão e a bandeira Visa, Mastercard, etc.
              * A máquina deve verificar a validade dos dados e informar se o pagamento foi aceito, ou não.
              * O procedimento de verificação deverá usar um sorteio para recusar alguns pagamentos, simulando uma situação real.*/
@@ -604,8 +635,8 @@ int main()
                 cout << "Insira a bandeira (Visa, Mastercard, Elo, Cielo, etc...: ";
                 cin >> bandeira;
                 //if (sizeof(num_cartao) == 8 && num_cartao%1000000 == 33)
-                if ((rand()%15)+1 > 2) // Processo para gerar um numero aleatorio entre 1 e 15. Caso seja maior que 2
-                                       // (cerca de 90% das vezes), o processo é concluido
+                if ((rand() % 15) + 1 > 2) // Processo para gerar um numero aleatorio entre 1 e 15. Caso seja maior que 2
+                                           // (cerca de 90% das vezes), o processo é concluido
                 {
                     cout << "\n\n\nCartao validado com sucesso. Pagamento realizado. \nConfirme suas informacoes antes de emitir o bilhete.\n\n";
 
@@ -641,15 +672,13 @@ int main()
                 else if (escolhe_evento == 3)
                     cout << "O valor do bilhete para o teatro eh: " << t1->getvalor() << ",00 R$";
 
-
                 cout << "\nInsira o numero do cartao: ";
                 cin >> num_cartao;
                 cout << "Insira a bandeira (Visa, Mastercard, Elo, Cielo, etc...: ";
                 cin >> bandeira;
 
-
-                if ((rand()%5)+1 > 1) // Processo para gerar um numero aleatorio entre 1 e 15. Caso seja maior que 2
-                                       // (cerca de 90% das vezes), o processo é concluido
+                if ((rand() % 5) + 1 > 1) // Processo para gerar um numero aleatorio entre 1 e 15. Caso seja maior que 2
+                                          // (cerca de 90% das vezes), o processo é concluido
                 {
                     cout << "Saldo suficiente na conta. Pagamento realizado. \nConfirme suas informacoes antes de emitir o bilhete.\n\n";
 
@@ -688,7 +717,7 @@ int main()
                     cout << "Insira o numero do bilhete de cortesia: \n";
                     cin >> bilhete_cortesia;
 
-                    if ((rand()%15)+1 > 2)
+                    if ((rand() % 15) + 1 > 2)
                     {
                         cout << "Bilhete de cortesia valido. \nConfirme suas informacoes antes de emitir o bilhete.\n\n";
                         if (escolhe_evento == 1)
@@ -718,7 +747,6 @@ int main()
                 }
             }
             /* Sessão para o usuario validar as informações que irão constar no bilhete e silicitar a impressão do mesmo. */
-
 
             //imprime_pagamento();
         }
